58c3aa68d97a76167eed2d97791731609fa944ef
Alimadcorp <alimad.co.ltd@gmail.com>
2024-12-30 10:20:16 +0500

    NextLevel, MainMenu and basic functionality

--- Assets/Scripts/Playerdeath.cs
+++ Assets/Scripts/Playerdeath.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI; // For UI elements
@@ -7,46 +9,142 @@ public class PlayerDeath : MonoBehaviour
 {
     [Header("Death Screen Settings")]
     public GameObject deathScreen;
-    public string MainMenuName;
+    [SerializeField] private TextMeshProUGUI deathmsg;
+    [SerializeField] private Image loadingImage;
 
-    void Start()
+    [SerializeField] private CanvasGroup overlay;
+    private bool loadingSmth = false;
+    public string MainMenuName;
+    private void Start()
     {
-        if (deathScreen != null)
+        StartCoroutine(InTransition());
+    }
+    private IEnumerator InTransition()
+    {
+        overlay.gameObject.SetActive(true);
+        for (float t = 1; t > 0; t -= Time.unscaledDeltaTime)
         {
-            deathScreen.SetActive(false);
+            overlay.alpha = t;
+            yield return null;
         }
+        overlay.alpha = 0;
+        overlay.gameObject.SetActive(false);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            StartCoroutine(Die());
+            StartCoroutine(Die("Depolarized"));
+        }
+        if (collision.gameObject.CompareTag("Border"))
+        {
+            StartCoroutine(Die("Out of Border"));
         }
     }
 
-    IEnumerator Die()
+    IEnumerator Die(string message)
     {
         deathScreen.SetActive(true);
         PlayerScript.Instance.SimilationStarted = false;
+        deathmsg.text = message;
         yield return new WaitForSeconds(2);
         Time.timeScale = 0f;
     }
-
+    public void NextLevel()
+    {
+        int currentLevel;
+        string levelName = SceneManager.GetActiveScene().name;
+        currentLevel = Int32.Parse(levelName.Substring(5));
+        Debug.Log(currentLevel);
+        PlayerPrefs.SetInt("levels", currentLevel);
+        if (currentLevel < 8)
+        {
+            LoadLevel("Level" + (currentLevel + 1).ToString());
+        }
+        else
+        {
+            LoadLevel("Leadeboard");
+        }
+    }
     public void Retry()
     {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().name));
         Time.timeScale = 1f;
     }
     public void BackToMenu()
     {
-        SceneManager.LoadScene(MainMenuName);
+        StartCoroutine(LoadLevel("Menu"));
         Time.timeScale = 1f;
-
     }
     public void QuitGame()
     {
+        StartCoroutine(Exit());
+    }
+    private IEnumerator Exit()
+    {
+        overlay.gameObject.SetActive(true);
+        for (float t = 0; t < 1; t += Time.unscaledDeltaTime)
+        {
+            overlay.alpha = t;
+            yield return null;
+        }
+        overlay.alpha = 1;
+        yield return new WaitForSecondsRealtime(0.5f);
         Application.Quit();
-        Debug.Log("quit");
+    }
+    private IEnumerator LoadLevel(string name)
+    {
+        loadingSmth = true;
+        float progress = 0f;
+        overlay.gameObject.SetActive(true);
+        overlay.alpha = 0;
+        yield return new WaitForSecondsRealtime(0.2f);
+        while (progress < 0.05f)
+        {
+            progress = Mathf.Lerp(progress, 0.105f, Time.unscaledDeltaTime * 5f);
+            loadingImage.fillAmount = progress;
+            yield return null;
+        }
+        int lastScene = SceneManager.GetActiveScene().buildIndex;
+        AsyncOperation loading = SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+        loading.allowSceneActivation = false;
+        while (loading.progress < 0.9f)
+        {
+            if (progress < loading.progress)
+            {
+                progress = Mathf.Lerp(progress, loading.progress, Time.unscaledDeltaTime);
+            }
+            loadingImage.fillAmount = progress;
+            yield return null;
+        }
+        while (progress < 0.98f)
+        {
+            progress = Mathf.Lerp(progress, 1, Time.unscaledDeltaTime * 3f);
+            loadingImage.fillAmount = progress;
+            yield return null;
+        }
+        yield return new WaitForSecondsRealtime(0.3f);
+        for (float t = 0; t <= 1; t += Time.unscaledDeltaTime * 2f)
+        {
+            progress = Mathf.Lerp(progress, 1, Time.unscaledDeltaTime);
+            loadingImage.fillAmount = progress;
+            yield return null;
+        }
+        progress = 1f;
+        loadingImage.fillAmount = progress;
+        for (float t = 0; t <= 1; t += Time.unscaledDeltaTime * 2f)
+        {
+            overlay.alpha = t;
+            yield return null;
+        }
+        overlay.alpha = 1;
+        yield return new WaitForSecondsRealtime(0.2f);
+
+        loading.allowSceneActivation = true;
+        yield return new WaitUntil(() => loading.isDone);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(name));
+        loadingSmth = false;
+        SceneManager.UnloadSceneAsync(lastScene);
     }
 }
